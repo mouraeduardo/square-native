7038568b0163b39ad313b47db50652a5a93c4965
mouraeduardo <eduardomoura2001@hotmail.com>
2023-09-17 21:14:38 -0300

    first commit

--- App.js
+++ App.js
@@ -4,9 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.quadradoOne}>
+        <Text style={styles.textoQuadrado}>Mensagem no Quadrado</Text>
+      </View>
+      <View style={styles.quadradoTwo}>
+        <Text style={styles.textoQuadrado}>Mensagem no Quadrado</Text>
+      </View>
+      <View style={styles.quadradoThree}>
+        <Text style={styles.textoQuadrado}>Mensagem no Quadrado</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
@@ -14,7 +23,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },   
+  quadradoOne: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  quadradoTwo: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    textAlign: 'center',
+    
+  },
+  quadradoThree: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  textoQuadrado: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
   },
 });
